202e90e08624345360a9151e9dae480952883c50
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2021-10-05 17:51:35 +0300

    Update error_handler.py

--- handlers/errors/error_handler.py
+++ handlers/errors/error_handler.py
@@ -20,17 +20,21 @@ async def errors_handler(update, exception):
 
     if isinstance(exception, CantDemoteChatCreator):
         logging.exception("Can't demote chat creator")
+        # do something here?
         return True
 
     if isinstance(exception, MessageNotModified):
         logging.exception('Message is not modified')
+        # or here
         return True
     if isinstance(exception, MessageCantBeDeleted):
         logging.exception('Message cant be deleted')
+        # or here
         return True
 
     if isinstance(exception, MessageToDeleteNotFound):
         logging.exception('Message to delete not found')
+        # well, you know.
         return True
 
     if isinstance(exception, MessageTextIsEmpty):
@@ -45,14 +49,18 @@ async def errors_handler(update, exception):
         logging.exception(f'InvalidQueryID: {exception} \nUpdate: {update}')
         return True
 
-    if isinstance(exception, TelegramAPIError):
-        logging.exception(f'TelegramAPIError: {exception} \nUpdate: {update}')
-        return True
     if isinstance(exception, RetryAfter):
         logging.exception(f'RetryAfter: {exception} \nUpdate: {update}')
         return True
+      
     if isinstance(exception, CantParseEntities):
         logging.exception(f'CantParseEntities: {exception} \nUpdate: {update}')
         return True
+      
+    #  MUST BE THE  LAST CONDITION (ЭТО УСЛОВИЕ ВСЕГДА ДОЛЖНО БЫТЬ В КОНЦЕ)
+    if isinstance(exception, TelegramAPIError):
+        logging.exception(f'TelegramAPIError: {exception} \nUpdate: {update}')
+        return True
     
+    # At least you have tried.
     logging.exception(f'Update: {update} \n{exception}')
